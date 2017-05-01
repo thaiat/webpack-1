46c9a6b32b623f0933df8b3f8236be79be964a71
Avi Haiat <thaiat@ipelia.fr>
2017-05-01 19:05:56 +0300

    fix(app): Fix axios plugin

--- template/src/plugins/axiosPlugin.js
+++ template/src/plugins/axiosPlugin.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 // Usage
 // import axiosPlugin from './axiosPlugin.js';
 // Vue.use(axiosPlugin);
+// or
+// Vue.use(axiosPlugin, '$axios');
 
-export default (Vue) => {
-  Object.defineProperties(Vue.prototype, {
-    $http: {
-        return axios;
-    }
-  })
+export default {
+  install: function(Vue, name = '$http') {
+    Object.defineProperty(Vue.prototype, name, { value: axios });
+  }
 };
