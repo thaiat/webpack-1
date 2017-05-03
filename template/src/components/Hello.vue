b86cb3ed781b4d3b9e7c5bf09b80749900eacdf0
Avi Haiat <thaiat@ipelia.fr>
2017-05-03 19:22:15 +0300

    feat(app): Add roboto and font-awesome

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <img src="../assets/logo.svg" />
+    <img src="../assets/images/logo.svg" />
     <h1>\{{ msg }}</h1>
   </div>
 </template>
@@ -18,21 +18,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- template/src/main.js
+++ template/src/main.js
@@ -4,6 +4,7 @@
 {{/if_eq}}
 import Vue from 'vue';
 import App from './App';
+import './assets/styles/app.scss';
 {{#router}}
 import router from './router';
 {{/router}}
