26ff6b592320b4051e7b8aafbaaf27fb83fbe458
Avi Haiat <thaiat@ipelia.fr>
2017-05-03 14:48:26 +0300

    fix(app): Fix warning with unit tests

--- template/test/unit/index.js
+++ template/test/unit/index.js
@@ -9,5 +9,8 @@ testsContext.keys().forEach(testsContext);
 // require all src files except main.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-const srcContext = require.context('../../src', true, /(^\.\/(?!(main\.js|index\.html)?$))/);
-srcContext.keys().forEach(srcContext);
+const srcContext = require.context('../../src', true, /(^\.\/(?!(main\.js|main|index\.html)?$))/);
+srcContext.keys().forEach(key => {
+  // console.log('KEY', key)
+  srcContext(key);
+});
