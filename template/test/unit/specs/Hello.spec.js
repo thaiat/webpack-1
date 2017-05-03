6d77d754cf865d9d9e00f072de66bfff13423d42
Avi Haiat <thaiat@ipelia.fr>
2017-05-03 23:39:39 +0300

    feat(app): Modify unit test

--- template/test/unit/specs/Hello.spec.js
+++ template/test/unit/specs/Hello.spec.js
@@ -1,10 +1,25 @@
 import Vue from 'vue';
 import Hello from '@/components/Hello';
 
-describe('Hello.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent).to.equal('Welcome to Your Vue.js App');
+describe('Components', () => {
+  describe('Hello.vue', () => {
+
+    it('should render correct contents', () => {
+      const vm = new Vue({
+        template: `
+        <div>
+          <test></test>
+        </div>
+        `,
+        components: {
+          test: Hello
+        }
+      }).$mount();
+
+      return Vue.nextTick()
+        .then(() => {
+          expect(vm.$el.querySelector('.hello h1').textContent).to.equal('Welcome to Your Vue.js App');
+        });
+    });
   });
 });
